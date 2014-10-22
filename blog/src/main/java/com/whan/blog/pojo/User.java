670ef2853af8d4d938d3d0a07230df54e37ea12e
xingkong1221 <xingkongphp@gmail.com>
2014-10-23 01:24:41 +0800

    Normal commit

--- blog/src/main/java/com/whan/blog/pojo/User.java
+++ blog/src/main/java/com/whan/blog/pojo/User.java
@@ -1,9 +1,43 @@
 package com.whan.blog.pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
 /**
+ * 用户
  * Created by xingkong1221 on 2014/10/22.
  */
-public class User {
+@Entity
+public class User implements Serializable {
 
+    private static final long serialVersionUID = 4295016361385273791L;
+
+    /*用户编号*/
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    private String email;
+
+    private String url;
+
+    private String registered;
+
+    private String activationKey;
+
+    private String status;
+
+    private Date displayName;
+
+
 }
